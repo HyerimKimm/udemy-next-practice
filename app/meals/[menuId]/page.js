044166ffc7f55d6f7f4235391a43df5bdcf5be68
HyerimKimm <50258232+HyerimKimm@users.noreply.github.com>
2024-07-20 23:23:36 +0900

    feat: not found page, meal detail 페이지 구현

--- app/meals/[menuId]/page.js
+++ app/meals/[menuId]/page.js
@@ -1,3 +1,31 @@
-export default function MealsDetailPage({ params }) {
-  return <main>This is {params.menuId} Meals Page.</main>;
+import { getMeal } from "@/lib/meals";
+import classes from "./page.module.css";
+import Image from "next/image";
+
+export default async function MealsDetailPage({ params }) {
+  const meal = await getMeal(params.menuId);
+
+  meal.instructions = meal.instructions.replace(/\n/g, "<br />");
+
+  return (
+    <>
+      <header className={classes.header}>
+        <div className={classes.image}>
+          <Image src={meal.image} fill />
+        </div>
+        <div className={classes.headerText}>
+          <h1>{meal.title}</h1>
+          <p className={classes.creator}>
+            by <a href={`mailto:${meal.creator_email}`}>{meal.creator}</a>
+          </p>
+        </div>
+      </header>
+      <main>
+        <p
+          className={classes.instructions}
+          dangerouslySetInnerHTML={{ __html: meal.instructions }}
+        ></p>
+      </main>
+    </>
+  );
 }
---
+++ app/not-found.js
@@ -0,0 +1,8 @@
+export default function NotFound() {
+  return (
+    <main className="not-found">
+      <h1>404</h1>
+      <p>요청하신 페이지는 없는 페이지입니다.</p>
+    </main>
+  );
+}
